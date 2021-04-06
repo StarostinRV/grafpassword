8742bd83b3addc3df9de1701b1f560e35a280d00
StarostinRV <r.starostin2000@yandex.ru>
2021-04-06 09:07:26 +0400

    Node v01

--- grafpassword/GrafPanel.cs
+++ grafpassword/GrafPanel.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace grafpassword
 {
     class GrafPanel
     {
+        int dimension_matrix;
+        Node[,] nodes;
+        bool finish;
+        string password;
+        int now_index;
+        Form1 form1;
+        Point location;
+        Size size;
+        int[,] grafmodel;
     }
 }
--- grafpassword/Node.cs
+++ grafpassword/Node.cs
@@ -1,12 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace grafpassword
 {
     class Node
     {
+        private static int _R;
+        private static int _r;
+        private Point _location;
+        private bool _active;
+
+        public static int R
+        {
+            get
+            {
+                return _R;
+            }
+
+            set
+            {
+                _R = value;
+            }
+        }
+
+        public static int r
+        {
+            get
+            {
+                return _r;
+            }
+
+            set
+            {
+                _r = value;
+            }
+        }
+
+        public bool Аctive
+        {
+            get
+            {
+                return _active;
+            }
+
+            set
+            {
+                _active = value;
+            }
+        }
+
+        public Point Location
+        {
+            get
+            {
+                return _location;
+            }
+
+            set
+            {
+                _location = value;
+            }
+        }
+
     }
 }
